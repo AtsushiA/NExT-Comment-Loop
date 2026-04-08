181481002d39cca6ce6e39ae381334234036ecc9
Atsushi Ando <ando@next-season.net>
2026-04-08 12:36:38 +0900

    chore: バージョンを 1.1.3 に更新

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => 'be0111b80e083d0e2dec');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '990ae947c5461da51533');
